65905348ab4fd47a35bd44ab560053079c8af6bd
denisagapov <denisagapov6666@gmail.com>
2024-01-21 19:59:26 +0100

    Slug config

--- members/admin.py
+++ members/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Member
+
 # Register your models here.
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ("firstname","lastname","phone","joined_date")
-admin.site.register(Member,MemberAdmin)
+  list_display = ("firstname", "lastname", "joined_date",)
+  prepopulated_fields = {"slug": ("firstname", "lastname")}
+  
+admin.site.register(Member, MemberAdmin)
