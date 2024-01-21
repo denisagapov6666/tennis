cd6a0db1d0a65175a95349b66e748bc0c217628f
denisagapov <denisagapov6666@gmail.com>
2024-01-21 18:06:00 +0100

    customize my project

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR/'productionfiles'
 STATIC_URL = 'static/'
 
+#Add this in your settings.py file
+
+STATICFILES_DIRS = [
+    BASE_DIR/'mystaticfiles'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
